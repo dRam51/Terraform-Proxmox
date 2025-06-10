36083449ad84ff2397be21f1b89fbfae2d3216be
Devin Ramotar <118420670+dRam51@users.noreply.github.com>
2025-06-09 21:13:59 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+}
+
+variable "proxmox_api_token_secret" {
+  type = string
+}
+
+variable "proxmox_node" {
+  type = string
+}
+
+variable "template_name" {
+  type = string
+}
+
+variable "storage_pool" {
+  type = string
+}
+
+variable "iso_image" {
+  type = string
+}
+
+variable "network_bridge" {
+  type = string
+}
+
+variable "pm_user" {
+  type = string
+}
+
+variable "pm_password" {
+  type = string
+}
+
+variable "ssh_public_key_path" {
+  type = string
+}
